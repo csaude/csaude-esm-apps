b906fbc03b23787530be0bcb3eadaf2768131ac9
Jerson Camara <jersondacamara@gmail.com>
2025-01-28 11:15:43 +0200

    check-box configuration in pregnancy step

--- packages/esm-consultation-workflow/src/steps/pregnancy-step.component.tsx
+++ packages/esm-consultation-workflow/src/steps/pregnancy-step.component.tsx
@@ -19,7 +19,12 @@ const PregnancyStep: StepFormComponent<Pregnancy> = ({ values, setValues }) => {
     <>
       <h4>Gravidez/Lactação/Planeamento familiar</h4>
       <StepForm values={values} onSubmit={onSubmit}>
-        <Obs name="pregnancy" conceptUuid="e1e056a6-1d5f-11e0-b929-000c29ad1d07" rendering="select" />
+        <Obs
+          name="pregnancy"
+          conceptUuid="e1e056a6-1d5f-11e0-b929-000c29ad1d07"
+          filterConceptUuid={['1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', '1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA']}
+          rendering="select"
+        />
         <Obs name="lactating" conceptUuid="bc4fe755-fc8f-49b8-9956-baf2477e8313" rendering="select" />
         <Obs
           name="lastMenstruationDate"
