fd9113e33a0f74e49549bdb9c4a4ff0b269efd68
Yassin Nurmahomed <yassin.nurmahomed@csaude.org.mz>
2025-03-06 10:08:31 +0200

    Use csaude form engine

--- packages/esm-consultation-workflow/src/dynamic-workflow/components/form-renderer.component.tsx
+++ packages/esm-consultation-workflow/src/dynamic-workflow/components/form-renderer.component.tsx
@@ -4,7 +4,7 @@ import styles from './form-renderer.scss';
 import { InlineLoading, Button } from '@carbon/react';
 import FormError from './form-error.component';
 import { useTranslation } from 'react-i18next';
-import { FormEngine, OpenmrsEncounter } from '@openmrs/esm-form-engine-lib';
+import { FormEngine, OpenmrsEncounter } from '@csaude/esm-form-engine-lib';
 import { launchPatientWorkspace } from '@openmrs/esm-patient-common-lib';
 import { closeWorkspace } from '@openmrs/esm-framework';
 import { StepComponentProps, WorkflowState, WorkflowStep } from '../types';
